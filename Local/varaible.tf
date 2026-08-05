5e1b26bd7ed60628d4f45399f055e87786fb5408
Tamilarasi08-Dev <tamilarasi2512@gmail.com>
2026-08-05 21:50:48 +0530

    Define variables for AWS configuration
    Added variables for AWS region, instance type, and AMI ID.

--- Local/varaible.tf
+++ Local/varaible.tf
@@ -1,1 +1,23 @@
+variable "aws_region" {
 
+  description = "AWS Region"
+
+  type = string
+
+}
+
+variable "instance_type" {
+
+  description = "EC2 Instance Type"
+
+  type = string
+
+}
+
+variable "ami_id" {
+
+  description = "Amazon Linux AMI"
+
+  type = string
+
+}
